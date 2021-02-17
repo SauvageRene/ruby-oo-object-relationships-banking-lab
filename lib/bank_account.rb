9b99a928780b67ccdbad1c306ecf40aa430281da
Rene <renegarcia1864@gmail.com>
2021-02-17 15:09:14 -0600

    commit tree cffe92566f57ada67e9829898c2f83e0e4cfeeaa

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,24 +7,24 @@ class BankAccount
         @status = 'open'
     end
 
-    def deposit(money)
-        self.balance += money
+    def deposit(amount)
+        self.balance += amount
+        self.balance
     end
+
     def display_balance
         "Your balance is $#{self.balance}."
     end
+
     def valid?
-        if self.status == 'open' && self.balance > 0
+        if self.balance > 0 && status == 'open'
             true
         else
             false
         end
-        
     end
 
     def close_account
         self.status = 'closed'
     end
-
-
 end
